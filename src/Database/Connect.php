0ea4dd062ee0ae7e0e7469b7fc2b71c05c345bb1
Wilton C <wiltonmeg4@gmail.com>
2022-01-31 14:37:58 -0300

    construindo banco

--- src/Database/Connect.php
+++ src/Database/Connect.php
@@ -2,16 +2,28 @@
 
 namespace Src\Database;
 
+use Exception;
+use PDO;
+use PDOException;
+use PDOStatement;
+
 class Connect
 {    
-    protected const HOST = "";
-    protected const DB_NAME = "";
-    protected const DB_USER = "";
+    protected const HOST = "localhost";
+    protected const PORT = "3306";
+    protected const DB_NAME = "acesso";
+    protected const DB_USER = "root";
     protected const DB_PASS = "";
 
-    public function connect ()
+    protected function connect()
     {
-        echo "estou nesse ponto";
+        try {
+            $conn = new PDO('mysql:host=' . self::HOST . ';port=' . self::PORT . ';dbname='. self::DB_NAME, self::DB_USER, self::DB_PASS);
+            echo "Tudo certo com o banco de dados";
+        } catch (PDOException $e){
+            echo "Erro: Erro ao conectar ao banco de dados";
+            return false;
+        }
     }
 
 }
